91a6a8fa3acec4827af8e3e07aaeee27723d73da
krok-code <maks.smolinskyi@gmail.com>
2024-05-03 14:20:25 +0300

    push

--- src/assets/fonts/images/icons/Close.jsx
+++ src/assets/fonts/images/icons/Close.jsx
@@ -1,4 +1,4 @@
-const Eye = ({ width, height, fillColor, strokeColor }) => {
+const Close = ({ width, height, fillColor, strokeColor }) => {
   return (
     <svg
       width={width || '14px'}
@@ -7,16 +7,17 @@ const Eye = ({ width, height, fillColor, strokeColor }) => {
       fill={fillColor || 'none'}
       stroke={strokeColor || null}
       xmlns="http://www.w3.org/2000/svg"
+      style={{ cursor: 'pointer' }}
     >
       <path
-        d="M0.75 9C0.75 9 3.75 3 9 3C14.25 3 17.25 9 17.25 9C17.25 9 14.25 15 9 15C3.75 15 0.75 9 0.75 9Z"
-        strokeWidth="1.3"
+        d="M13.5 4.5L4.5 13.5"
+        strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
-        d="M9 11.25C10.2426 11.25 11.25 10.2426 11.25 9C11.25 7.75736 10.2426 6.75 9 6.75C7.75736 6.75 6.75 7.75736 6.75 9C6.75 10.2426 7.75736 11.25 9 11.25Z"
-        strokeWidth="1.3"
+        d="M4.5 4.5L13.5 13.5"
+        strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
@@ -24,4 +25,4 @@ const Eye = ({ width, height, fillColor, strokeColor }) => {
   );
 };
 
-export default Eye;
+export default Close;
