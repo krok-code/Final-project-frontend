2e6de77bb1e88e108d7852ef5b8168c640c25743
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-05-03 13:46:12 +0100

    fix style

--- src/components/Auth/RegistrationPg/RegistrationPg.styled.jsx
+++ src/components/Auth/RegistrationPg/RegistrationPg.styled.jsx
@@ -17,13 +17,13 @@ export const Wrap = styled.div`
 
 export const Input = styled.input`
   padding: 14px 18px;
-  color: var(--white-color);
+  color: #fff;
   border-radius: 8px;
   font-size: 14px;
   font-weight: 400;
   line-height: normal;
   letter-spacing: -0.28px;
-  border: 1px solid var(--input-color);
+  border: 1px solid #bedbb0;
   opacity: 0.4;
   background: #1f1f1f;
   box-shadow: 0px 4px 16px 0px rgba(22, 22, 22, 0.08);
@@ -33,16 +33,16 @@ export const Input = styled.input`
     outline: none;
   }
   &::placeholder {
-    color: var(--white-color);
+    color: #fff;
   }
 
   ${props =>
     props.$error &&
     `
-    color: var(--error-color);
-    border-color: var(--error-color);
+    color: #fc8181;
+    border-color: #fc8181;
     &::placeholder {
-      color: var(--error-color);
+      color: #fc8181;
     }
   `}
   @media screen and (min-width: 320px) {
@@ -59,12 +59,12 @@ export const Input = styled.input`
 export const Error = styled.p`
   font-size: 12px;
   line-height: normal;
-  color: var(--error-color);
+  color: #fc8181;
 `;
 
 export const Button = styled.button`
   width: 100%;
-  color: var(--black-color);
+  color: #161616;
   text-align: center;
   border: none;
   text-decoration: none;
@@ -78,7 +78,7 @@ export const Button = styled.button`
   padding: 14px;
   &:hover {
     transition: opacity 200ms linear;
-    background-color: var(--accent-color);
+    background-color: #9dc888;
   }
 `;
 export const Form = styled.form`
