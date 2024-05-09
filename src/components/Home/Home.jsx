8639382795705cc12a10b9cefef82f11e214e992
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-05-09 15:58:56 +0100

    add component board

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,13 +2,19 @@
 // import { Outlet, useLocation } from 'react-router-dom';
 // import { useSelector } from 'react-redux';
 
+import { useTheme } from '@mui/material';
 import Header from '../Header/Header';
 // import ScreensPage from '../../components/ScreensPage/ScreensPage';
 
 import { HomePageContainer, MainContainer } from './Home.styled';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme } from '../../assets/theme/theme';
+
+import Board from 'pages/ScreensPage/Board/Board';
 // import { MainWrapper } from '../../components/ScreensPage/ScreensPage.styled';
 
 const HomePage = () => {
+  const theme = useTheme();
   //   const [aside, setAside] = useState(false);
   //   const location = useLocation();
 
@@ -19,18 +25,19 @@ const HomePage = () => {
   //   }
 
   return (
-    <HomePageContainer>
-      <p> Sidebar component</p>
-      <MainContainer>
-        <Header />
-        {/* <MainWrapper
+    <ThemeProvider theme={darkTheme}>
+      <HomePageContainer>
+        <MainContainer>
+          <Header /> <Board />
+          {/* <MainWrapper
         background={board ? board.background : 'default'}
         loc={location.pathname}
         >
           {location.pathname === '/home' ? <ScreensPage /> : <Outlet />}
         </MainWrapper> */}
-      </MainContainer>
-    </HomePageContainer>
+        </MainContainer>
+      </HomePageContainer>
+    </ThemeProvider>
   );
 };
 
