a48fd0aff69c572a17633bf090ed17e644b0873c
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2024-05-09 11:12:00 +0100

    selector, slice , home

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,8 +1,8 @@
 export const selectIsLoading = state => state.isLoading;
-export const selectError = state => state.users.error;
-export const selectIsLoggedIn = state => state.isLoggedIn;
-export const selectToken = state => state.users.token;
-export const selectUserData = state => state.users.userData;
+export const selectError = state => state.auth.error;
+export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+export const selectToken = state => state.auth.token;
+export const selectUserData = state => state.auth.userData;
 export const selectColumns = state => state.boards.columns;
-export const selectIsRefreshing = state => state.isRefreshing;
+export const selectIsRefreshing = state => state.auth.isRefreshing;
 export const selectTheme = state => state.theme;
