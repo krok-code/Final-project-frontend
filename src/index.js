04f3587a81c39de3583e17df11be48f038a163e8
krok-code <maks.smolinskyi@gmail.com>
2024-05-13 19:17:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import '../src/assets/i18/i18';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/task-pro">
+      <BrowserRouter basename="/Final-project-frontend">
         <GlobalStyles />
         <App />
       </BrowserRouter>
